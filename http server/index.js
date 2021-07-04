5d2d99765e94808a3c3cd4d98da24702f60a4c79
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2021-07-04 18:31:37 +0500

    API

--- http server/index.js
+++ http server/index.js
@@ -1,20 +1,23 @@
 const http = require('http');
-// const url = require('url');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
+
+    const data = fs.readFileSync(`${__dirname}/userAPI/userapi.json`, "utf-8");
+    const objdata = JSON.parse(data);
     if (req.url == "/") {
         res.end("Hey! I am Response");
     } else if (req.url == "/about") {
         res.end("Hey! I am About us Page");
     }
     else if (req.url == "/userapi") {
-        res.write("Hello from USER API");
-        res.end();
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(objdata[0].id + " " + objdata[0].name);
     } else {
         res.writeHead(404, { "content-type": "text/html" });
         res.end("<h1>404: Page Not Found</h1>");
     }
 })
 
-server.listen(8001, "127.0.0.1", () => {
+server.listen(8000, "127.0.0.1", () => {
     console.log("I am API Server");
 })
