e1bb3a2202dfb19d6402850d23fc212c84ba6259
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2021-07-04 14:08:00 +0500

    Module Wrapper Function --> how data become private?

---
+++ Module Wrapper Function/index.js
@@ -0,0 +1,26 @@
+// Node JS #15: Module Wrapper Function in Node.JS
+// () grouping operator
+
+
+// (function (exports, require, module, _filename, _dirname) {
+//     const a = require("fs");
+//     module.exports = { a, b, c, d };
+// }) //Module wrapper function
+
+
+
+//jub b run kartay han to nodejs hmaray modules ko ak wrapper man wrap kar data jis ki waja sa wo private ho jatay han
+
+
+
+(function () {
+    const name = "Arbab";
+    console.log(name);
+})();
+
+
+
+
+
+
+
